05a8ea37f36440de7e8ccf737f51ab4eebbc9d73
smaranan <seb.maranan@gmail.com>
2017-06-06 14:39:57 +0200

    Add files via upload

---
+++ Matrices.java
@@ -0,0 +1,13 @@
+package com.marananseb.outilmathematique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Matrices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrices);
+    }
+}
